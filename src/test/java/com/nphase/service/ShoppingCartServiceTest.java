6659355e1aba7ccdd195267b1a9e6906b07c4370
DenysovDenys <denysovdenyswork@gmail.com>
2022-09-07 18:35:21 +0300

    Fixed tests

--- src/test/java/com/nphase/service/ShoppingCartServiceTest.java
+++ src/test/java/com/nphase/service/ShoppingCartServiceTest.java
@@ -7,7 +7,6 @@ import com.nphase.entity.ShoppingCart;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import javax.naming.OperationNotSupportedException;
 import java.math.BigDecimal;
 import java.util.Arrays;
 
@@ -23,7 +22,7 @@ public class ShoppingCartServiceTest {
 
         BigDecimal result = service.calculateTotalPrice(cart);
 
-        Assertions.assertEquals(result, BigDecimal.valueOf(16.5));
+        Assertions.assertEquals(BigDecimal.valueOf(16.5), result);
     }
 
     @Test
@@ -35,7 +34,7 @@ public class ShoppingCartServiceTest {
 
         BigDecimal result = service.calculateTotalPriceWithDiscount(cart);
 
-        Assertions.assertEquals(result, BigDecimal.valueOf(33.0));
+        Assertions.assertEquals(BigDecimal.valueOf(33.0).stripTrailingZeros(), result.stripTrailingZeros());
     }
 
     @Test
@@ -43,11 +42,11 @@ public class ShoppingCartServiceTest {
         ShoppingCart cart = new ShoppingCart(Arrays.asList(
                 new Product("Tea", BigDecimal.valueOf(5.3), 2, Category.DRINK),
                 new Product("Coffee", BigDecimal.valueOf(3.5), 2, Category.DRINK),
-                new Product("Cheese", BigDecimal.valueOf(8), 2, Category.DRINK)
+                new Product("Cheese", BigDecimal.valueOf(8), 2, Category.FOOD)
         ));
 
         BigDecimal result = service.calculateTotalPriceWithDiscountByCategory(cart);
 
-        Assertions.assertEquals(result, BigDecimal.valueOf(31.84));
+        Assertions.assertEquals(BigDecimal.valueOf(31.84), result);
     }
 }
